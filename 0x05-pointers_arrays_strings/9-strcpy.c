9fbecf6ef0bd4cef628eadfdaef7f9ebad55fa70
munyemana <munyemanaprincerichard@gmail.com>
2023-06-26 14:55:52 -0700

    9-strcpy.c

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -11,11 +11,12 @@ char *_strcpy(char *dest, char *src)
 	int l = 0;
 	int x = 0;
 
-	while (*(src + 1) != '\0')
-	{
+	while (src != '\0')
 		l++;
+	{
+		src++;
 	}
-	for ( ; x < l ; x++)
+	for ( ; x <= l ; x++)
 	{
 		dest[x] = src[x];
 	}
